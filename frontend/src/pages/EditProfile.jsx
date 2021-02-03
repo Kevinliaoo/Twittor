16b21702e16109eb5e9a2676f0cac0ad16eef2c8
Kevin Liao <liaok0082@gmail.com>
2021-02-03 11:13:57 -0300

    Profile section

--- frontend/src/pages/EditProfile.jsx
+++ frontend/src/pages/EditProfile.jsx
@@ -10,7 +10,9 @@ import config from '../config';
 import '../assets/styles/pages/EditProfile.css';
 
 const EditProfile = props => {
-    const [form, setform] = useState({}); 
+    const [form, setform] = useState({
+        uid: localStorage.getUser()._id
+    }); 
 
     const handleClick = event => {
         setform({
@@ -43,13 +45,11 @@ const EditProfile = props => {
                 username: form.username.toLowerCase().trim(), 
             })
         }
-        setform({
-            uid: localStorage.getUser()._id,
-        })
         const config_ = {
             headers: { Authorization: `Bearer ${localStorage.getJwt()}` }
         }; 
         const URL = `${config.host_URL}/api/users`; 
+        
         axios.put(URL, form, config_) 
             .then(() => {
                 localStorage.reloadUserdata();
@@ -68,70 +68,72 @@ const EditProfile = props => {
             <div className="left">
                 <Navbar />
             </div>
-            <div className="edit_profile_content">
-                <div className="tittle_wrapper">
-                    <h2>Edit profile: </h2>
-                </div>
-                <form className="edit_profile__form">
-                    <input
-                        className = "form__input"
-                        onChange = {handleClick}
-                        name = "firstName"
-                        placeholder = "First name: "
-                    />
-                    <input
-                        className = "form__input"
-                        onChange = {handleClick}
-                        name = "lastName"
-                        placeholder = "Last name: "
-                    />
-                    <input
-                        className = "form__input"
-                        onChange = {handleClick}
-                        name = "username"
-                        placeholder = "Username: "
-                    />
-                    <input
-                        className = "form__input"
-                        onChange = {handleClick}
-                        name = "password"
-                        type = "password"
-                        placeholder = "New password: "
-                    />
-                    <input
-                        className = "form__input"
-                        onChange = {handleClick}
-                        name = "repeatPsw"
-                        type = "password"
-                        placeholder = "Repeat new password: "
-                    />
-                    <input
-                        className = "form__input"
-                        onChange = {handleClick}
-                        name = "profileURL"
-                        placeholder = "Profile picture URL: "
-                    />
-                    <input
-                        className = "form__input"
-                        onChange = {handleClick}
-                        name = "portraitURL"
-                        placeholder = "Portrait picture URL: "
-                    />
-                    <textarea
-                        className = "form__textarea"
-                        onChange = {handleClick} 
-                        name = "bio"
-                        placeholder = "Biography: "
-                    />
-                    <div className="form__buttons">
-                        <button className="save_button" onClick={saveChanges}>
-                            Save
-                        </button>
-                        <button className="cancel_button" onClick={goBack}>
-                            Cancel
-                        </button>
+            <div className="middle">
+                <div className="edit_profile_content">
+                    <div className="tittle_wrapper">
+                        <h2>Edit profile: </h2>
                     </div>
-                </form>
+                    <form className="edit_profile__form">
+                        <input
+                            className = "form__input"
+                            onChange = {handleClick}
+                            name = "firstName"
+                            placeholder = "First name: "
+                        />
+                        <input
+                            className = "form__input"
+                            onChange = {handleClick}
+                            name = "lastName"
+                            placeholder = "Last name: "
+                        />
+                        <input
+                            className = "form__input"
+                            onChange = {handleClick}
+                            name = "username"
+                            placeholder = "Username: "
+                        />
+                        <input
+                            className = "form__input"
+                            onChange = {handleClick}
+                            name = "password"
+                            type = "password"
+                            placeholder = "New password: "
+                        />
+                        <input
+                            className = "form__input"
+                            onChange = {handleClick}
+                            name = "repeatPsw"
+                            type = "password"
+                            placeholder = "Repeat new password: "
+                        />
+                        <input
+                            className = "form__input"
+                            onChange = {handleClick}
+                            name = "profileURL"
+                            placeholder = "Profile picture URL: "
+                        />
+                        <input
+                            className = "form__input"
+                            onChange = {handleClick}
+                            name = "portraitURL"
+                            placeholder = "Portrait picture URL: "
+                        />
+                        <textarea
+                            className = "form__textarea"
+                            onChange = {handleClick} 
+                            name = "bio"
+                            placeholder = "Biography: "
+                        />
+                        <div className="form__buttons">
+                            <button className="save_button" onClick={saveChanges}>
+                                Save
+                            </button>
+                            <button className="cancel_button" onClick={goBack}>
+                                Cancel
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
             <div className="right">
                 <Search>
